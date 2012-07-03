a9d27fc0aabcdf053f787e9742a8d9d3ca5e6ee4
Florent Jaouali <flornet@gmail.com>
2012-07-03 07:05:12 +0200

    Ruby version problem

--- app/controllers/admin/dive_clubs_controller.rb
+++ app/controllers/admin/dive_clubs_controller.rb
@@ -8,7 +8,7 @@ class Admin::DiveClubsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @admin_dive_clubs }
+      format.json { render :json => @admin_dive_clubs }
     end
   end
 
@@ -19,7 +19,7 @@ class Admin::DiveClubsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @admin_dive_club }
+      format.json { render :json => @admin_dive_club }
     end
   end
 
@@ -30,7 +30,7 @@ class Admin::DiveClubsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @admin_dive_club }
+      format.json { render :json => @admin_dive_club }
     end
   end
 
@@ -46,11 +46,11 @@ class Admin::DiveClubsController < ApplicationController
 
     respond_to do |format|
       if @admin_dive_club.save
-        format.html { redirect_to @admin_dive_club, notice: 'Dive club was successfully created.' }
-        format.json { render json: @admin_dive_club, status: :created, location: @admin_dive_club }
+        format.html { redirect_to @admin_dive_club, :notice => 'Dive club was successfully created.' }
+        format.json { render :json => @admin_dive_club, :status => :created, :location => @admin_dive_club }
       else
-        format.html { render action: "new" }
-        format.json { render json: @admin_dive_club.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.json { render :json => @admin_dive_club.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -62,11 +62,11 @@ class Admin::DiveClubsController < ApplicationController
 
     respond_to do |format|
       if @admin_dive_club.update_attributes(params[:admin_dive_club])
-        format.html { redirect_to @admin_dive_club, notice: 'Dive club was successfully updated.' }
+        format.html { redirect_to @admin_dive_club, :notice => 'Dive club was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
-        format.json { render json: @admin_dive_club.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.json { render :json => @admin_dive_club.errors, :status => :unprocessable_entity }
       end
     end
   end
